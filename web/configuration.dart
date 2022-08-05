08a030d93183434e7ea0a4e517fe01c0d1d83c31
laurentiu <laurentiu.luca@migratorydata.com>
2022-08-05 15:46:00 +0300

    update scores from subject image.

--- web/configuration.dart
+++ web/configuration.dart
@@ -3,59 +3,82 @@ import 'dart:math';
 final token = 'some-token';
 final encryption = false;
 final server = 'demo.migratorydata.com';
-final subject = '/games/football/all';
+final subjectPrefix = '/games/football/';
 
 final rng = Random();
 
-var matches = [
-  {
+var matches = {
+  'id1': {
     'id': 'id1',
     'home_team': 'Fenerbahce (Tur)',
     'away_team': 'Partizan (Srb)',
+    'score_home': 0,
+    'score_away': 0,
   },
-  {
+  'id2': {
     'id': 'id2',
     'home_team': 'Olympiakos (Gre)',
     'away_team': 'Slovan Bratislava (Svk)',
+    'score_home': 0,
+    'score_away': 0,
   },
-  {
+  'id3': {
     'id': 'id3',
     'home_team': 'Benfica (Por)',
     'away_team': 'Midtjylland (Den)',
+    'score_home': 0,
+    'score_away': 0,
   },
-  {
+  'id4': {
     'id': 'id4',
     'home_team': 'PSV (Ned)',
     'away_team': 'Monaco (Fra)',
+    'score_home': 0,
+    'score_away': 0,
   },
-  {
+  'id5': {
     'id': 'id5',
     'home_team': 'Guimaraes (Por)',
     'away_team': 'Hajduk Split (Cro)',
+    'score_home': 0,
+    'score_away': 0,
   },
-  {
+  'id6': {
     'id': 'id6',
     'home_team': 'Malmo FF (Swe)',
     'away_team': 'Dudelange (Lux)',
+    'score_home': 0,
+    'score_away': 0,
   },
-  {
+  'id7': {
     'id': 'id7',
     'home_team': 'Maribor (Slo)',
     'away_team': 'HJK (Fin)',
+    'score_home': 0,
+    'score_away': 0,
   },
-  {
+  'id8': {
     'id': 'id8',
     'home_team': 'Paide (Est)',
     'away_team': 'Anderlecht (Bel)',
+    'score_home': 0,
+    'score_away': 0,
   },
-  {
+  'id9': {
     'id': 'id9',
     'home_team': 'Neftci Baku (Aze)',
     'away_team': 'Rapid Viena (Aut)',
+    'score_home': 0,
+    'score_away': 0,
   },
-  {
+  'id10': {
     'id': 'id10',
     'home_team': 'Soligorsk (Blr)',
     'away_team': 'CFR Cluj (Rou)',
+    'score_home': 0,
+    'score_away': 0,
   },
-];
+};
+
+var matchesKeys = matches.keys.toList();
+var subjects = matchesKeys.map((e) => subjectPrefix + e).toList();
--- web/main.dart
+++ web/main.dart
@@ -24,8 +24,8 @@ void main() {
 void createMatchesTable() {
   var tBody = tableMatches.createTBody();
 
-  matches.asMap().forEach((index, element) {
-    var row = tBody.insertRow(index);
+  matches.forEach((index, element) {
+    var row = tBody.addRow();
     row.id = element['id'] as String;
     row.insertCell(0).text = element['home_team'] as String;
     row.insertCell(1).text = '0';
@@ -37,6 +37,7 @@ void createMatchesTable() {
 
 void startSimulation(Event e) {
   if (simulationRunning == false) {
+    writeToConsole('Simulation started!\n');
     simulationRunning = true;
     Timer.periodic(Duration(seconds: 2), (Timer t) {
       scoreSimulation();
@@ -46,13 +47,18 @@ void startSimulation(Event e) {
 
 void scoreSimulation() {
   for (var i = 0; i < rng.nextInt(5); i += 1) {
-    var match = matches[rng.nextInt(matches.length)];
+    var match = matches[matchesKeys[rng.nextInt(matches.length)]];
+    var scoreUpdate = rng.nextInt(2) == 0 ? 'score_home' : 'score_away';
+    match![scoreUpdate] = (match[scoreUpdate] as int) + 1;
     var content = json.encode({
       'id': match['id'],
-      'update': rng.nextInt(2) == 0 ? 'score_home' : 'score_away'
+      'score_home': match['score_home'],
+      'score_away': match['score_away'],
+      'goal': scoreUpdate
     });
     migratoryDataClient.publish(MigratoryDataMessage(
-        subject, Uint8List.fromList(utf8.encode(content))));
+        subjectPrefix + (match['id'] as String),
+        Uint8List.fromList(utf8.encode(content))));
   }
 }
 
@@ -61,25 +67,31 @@ void connectClient() {
   migratoryDataClient.setListener(ListenerImpl());
   migratoryDataClient.setEncryption(encryption);
   migratoryDataClient.setServers([server]);
-  migratoryDataClient.subscribe([subject]);
+  migratoryDataClient.subscribe(subjects);
   migratoryDataClient.connect();
+}
+
+void writeToConsole(data) {
+  console.appendText(data);
+  console.scrollTop = console.scrollHeight;
 }
 
 class ListenerImpl implements MigratoryDataListener {
   @override
   void onMessage(MigratoryDataMessage message) {
-    if (message.getMessageType() == MessageType.UPDATE) {
-      var update = json.decode(utf8.decode(message.content));
-      var row = querySelector('#' + update['id']) as TableRowElement;
-      if (update['update'] == 'score_home') {
-        var oldScore = int.parse(row.cells[1].text!);
-        row.cells[1].text = (oldScore + 1).toString();
+    var update = json.decode(utf8.decode(message.content));
+    var row = querySelector('#' + update['id']) as TableRowElement;
 
+    row.cells[1].text = (update['score_home'] % 10).toString();
+    row.cells[3].text = (update['score_away'] % 10).toString();
+
+    matches[update['id']]!['score_home'] = update['score_home'] % 10;
+    matches[update['id']]!['score_away'] = update['score_away'] % 10;
+
+    if (message.getMessageType() == MessageType.UPDATE) {
+      if (update['goal'] == 'score_home') {
         flash(row.cells[0], row.cells[1]);
       } else {
-        var oldScore = int.parse(row.cells[3].text!);
-        row.cells[3].text = (oldScore + 1).toString();
-
         flash(row.cells[3], row.cells[4]);
       }
       writeToConsole('onMessage - $update\n');
@@ -99,10 +111,5 @@ class ListenerImpl implements MigratoryDataListener {
       cell.className = '';
       cell2.className = '';
     });
-  }
-
-  void writeToConsole(data) {
-    console.appendText(data);
-    console.scrollTop = console.scrollHeight;
   }
 }
